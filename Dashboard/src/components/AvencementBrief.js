8b575f09e3e859887314abfaad4e3115f6bd4fe7
tba-adnan <tebbaa.adnane.dev@gmail.com>
2023-01-16 09:06:12 +0100

    fix.

--- Dashboard/src/components/AvencementBrief.js
+++ Dashboard/src/components/AvencementBrief.js
@@ -16,7 +16,7 @@ render() {
     {this.props.data.map(item => (<><br></br>
     <i class="fa-solid fa-paperclip"></i>
     <span>{item?.Nom_du_Brief}</span>
-    <ProgressBar now={'50'} label={`${0}%`}/></>) )}
+    <ProgressBar now={'50'} label={`${50}%`}/></>) )}
     </div>
     </div>
     )
